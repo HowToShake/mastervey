da138a6489a7e946f9abbfc756249f58a2da1003
michal.ryskiewicz <michal.ryskiewicz@inclub.io>
2022-05-05 00:14:01 +0200

    [ADDED] React grid layout

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,76 +1,98 @@
-import Box from "@mui/material/Box";
-import { ReactElement, ReactNode, SyntheticEvent, useState } from "react";
+import "/node_modules/react-grid-layout/css/styles.css";
+import "/node_modules/react-resizable/css/styles.css";
+import { ReactElement, useState } from "react";
 import Navbar from "../components/Navbar";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
+import { WidthProvider, Responsive } from "react-grid-layout";
+import _ from "lodash";
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 import { useAuth } from "../hooks/useAuth";
-
-interface TabPanelProps {
-  children?: ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
 
 const Dashboard = () => {
   const { user } = useAuth();
   console.log("user", user);
-  const [value, setValue] = useState(0);
 
-  const handleChange = (event: SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+  const [state, setState] = useState({
+    items: [0, 1, 2, 3, 4].map(function (i, key, list) {
+      return {
+        i: i.toString(),
+        x: i * 2,
+        y: 0,
+        w: 2,
+        h: 2,
+        add: i === list.length - 1,
+      };
+    }),
+    newCounter: 0,
+  });
+
+  const onAddItem = () => {
+    setState({
+      ...state,
+      items: state.items.concat({
+        i: "n" + state.newCounter,
+        x: (state.items.length * 2) % (state.cols || 12),
+        y: Infinity,
+        w: 2,
+        h: 2,
+      }),
+
+      newCounter: state.newCounter + 1,
+    });
   };
+
+  const createElement = (el) => {
+    const removeStyle = {
+      position: "absolute",
+      right: "2px",
+      top: 0,
+      cursor: "pointer",
+    };
+    const i = el.add ? "+" : el.i;
+    return (
+      <div key={i} data-grid={el} style={{ backgroundColor: "red" }}>
+        {el.add ? (
+          <span
+            className="add text"
+            onClick={onAddItem}
+            title="You can add an item by clicking here, too."
+          >
+            Add +
+          </span>
+        ) : (
+          <span className="text">{i}</span>
+        )}
+        <span className="remove" style={removeStyle}>
+          x
+        </span>
+      </div>
+    );
+  };
+
+  const onBreakpointChange = (breakpoint, cols) => {
+    setState({
+      ...state,
+      breakpoint: breakpoint,
+      cols: cols,
+    });
+  };
+
+  const onLayoutChange = (layout) => {
+    setState({ ...state, layout: layout });
+  };
+
+  console.log("state", state);
+
   return (
-    <>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="Moje ankiety" {...a11yProps(0)} />
-          <Tab label="Stwórz ankiety" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-    </>
+    <ResponsiveReactGridLayout
+      onLayoutChange={onLayoutChange}
+      onBreakpointChange={onBreakpointChange}
+      className="layout"
+      cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+      rowHeight={100}
+    >
+      {_.map(state.items, (el) => createElement(el))}
+    </ResponsiveReactGridLayout>
   );
 };
 
